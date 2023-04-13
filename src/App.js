e8b993dbd85c03404c95c02fbcf868b0a6be5aea
Kenya Womack <ladykamillion27@gmail.com>
2023-04-12 22:25:19 -0500

    fixed part of modal

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Header from './components/header';
 import Main from './components/Main';
 import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
-render(){
-  return (
-    <div>
+  render(){
+    return (
+      <div>
       <Header></Header>
       <Main></Main>
      <Footer></Footer>
