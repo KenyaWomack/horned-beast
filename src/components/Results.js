74b213448f620cdd947ede96e4292b2d62c75c40
Kenya Womack <ladykamillion27@gmail.com>
2023-04-13 19:08:03 -0500

    fixed modal bootstrap styling

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,8 +6,16 @@ import React from 'react';
    render() {
     console.log(this.props.currentBeast);
      return (
-       <Card>
+       <Card style={{width:'20rem' }}>
          <Card.Img variant="top" src={this.props.currentBeast.image_url} />
+        <Card.Body>
+          <Card.Title>
+          {this.props.currentBeast.title}
+          </Card.Title>
+        </Card.Body>
+        <Card.Text>
+        {this.props.currentBeast.description}
+        </Card.Text>
        </Card>
      )
    }
